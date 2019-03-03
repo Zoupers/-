efcdfcd64f4959f240177e87f47ae9b4e4ae0e5c
dabaiandxiaohei <1018471226@qq.com>
2019-03-03 09:49:10 +0800

    ...

---
+++ wuyanweb/apps/user/email.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail,send_mass_mail
+from douban_movie import settings
+import random
+import string
+
+def random_str():
+    seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    sa = []
+    for i in range(6):
+        sa.append(random.choice(seed))
+    salt = ''.join(sa)
+    return salt
+
+#注册账号验证
+def send_A_email(req,name):
+    title = '云顶电影注册'
+    reciever = []
+    reciever.append(req)
+    random = random_str()
+    msg = '打开此链接激活账户：http://127.0.0.1:8000/active/?token='+random+'&name='+name
+    res = send_mail(title, msg,settings.EMAIL_HOST_USER, reciever)
+    return res,random
+
+#改密验证
+def send_B_email(req,name):
+    title = '云顶电影密码找回'
+    reciever = []
+    reciever.append(req)
+    random = random_str()
+    msg = '打开此链接激活账户：http://127.0.0.1:8000/reactive/?token='+random+'&name='+name
+    res = send_mail(title, msg,settings.EMAIL_HOST_USER, reciever)
+    return res,random
--- wuyanweb/apps/user/models.py
+++ wuyanweb/apps/user/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+#待激活中账户
+class Registing_User(models.Model):
+
+    name = models.CharField(max_length=128,unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    Email_code = models.CharField(max_length=128)
+
+#改密待验证中账户
+class Resetting_User(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=256)
+    Email_code = models.CharField(max_length=128)
+
+#已注册账户
+class User(models.Model):
+
+    name = models.CharField(max_length=128,unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    c_time = models.DateTimeField(auto_now_add=True)
+
+
+    class Mate:
+        ordering = ['c_time']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
+
+
